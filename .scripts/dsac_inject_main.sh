20dce903ce38051ac663ecb99f850e40f2a6594c
MagicalCodeMonkey <mwoodru@gmail.com>
2019-02-21 20:34:08 -0500

    Additional code to inject in DS main.sh

--- .scripts/dsac_inject_main.sh
+++ .scripts/dsac_inject_main.sh
@@ -10,6 +10,23 @@ dsac_inject_main() {
     
     if [[ $(grep -c "# DSAC injected code" $file_path) = 0 ]]; then
         info "Injecting code into DockSTARTer ${file}"
+
+        line_to_add_after="readonly DETECTED_HOMEDIR=\$(eval echo \"~\${DETECTED_UNAME}\" 2> /dev/null || true)"
+        line_number=$(($(grep -n "${line_to_add_after}" $file_path | sed 's/^\([0-9]\+\):.*$/\1/')+1))
+
+        lines_to_add=(
+            "# DSAC injected code"
+            "# DSAC Information"
+            "readonly DETECTED_DSACDIR=\$(eval echo \"~\${DETECTED_UNAME}/.docker/.dsac\" 2> /dev/null || true)"
+            "# /DSAC injected code"
+        )
+
+        for i in ${!lines_to_add[@]}; do
+            line_to_add=${lines_to_add[$i]}
+            sed -i "${line_number}i $line_to_add" $file_path
+            line_number=$((line_number+1))
+        done
+
         line_to_add_after="Failed to clone DockSTARTer repo to \${DETECTED_HOMEDIR}/.docker location."
         line_number=$(($(grep -n "${line_to_add_after}" $file_path | sed 's/^\([0-9]\+\):.*$/\1/')+1))
 
