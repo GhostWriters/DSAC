72cd8b91a1b666350fc5f3d300cdb734295dd87c
MagicalCodeMonkey <mwoodru@gmail.com>
2019-02-23 21:10:36 -0500

    Mostly changing variable names

--- .scripts/dsac_menu_config.sh
+++ .scripts/dsac_menu_config.sh
@@ -3,35 +3,31 @@ set -euo pipefail
 IFS=$'\n\t'
 
 dsac_menu_config() {
-    local CONFIGOPTS
-    CONFIGOPTS=()
-    CONFIGOPTS+=("Quick Setup " "- Little user input with pre-selected apps")
-    CONFIGOPTS+=("Full Setup " "- Full user input with no pre-selected apps")
+    local DSAC_CONFIGOPTS
+    DSAC_CONFIGOPTS=()
+    DSAC_CONFIGOPTS+=("Quick Setup " "= Little user input with pre-selected apps")
+    DSAC_CONFIGOPTS+=("Full Setup " "= Full user input with no pre-selected apps")
 
-    local CONFIGCHOICE
-    CONFIGCHOICE=$(whiptail --fb --clear --title "DockSTARTer App Config" --menu "What would you like to do?" 0 0 0 "${CONFIGOPTS[@]}" 3>&1 1>&2 2>&3 || echo "Cancel")
+    local DSAC_CONFIGCHOICE
+    CONFIGCHOICE=$(whiptail --fb --clear --title "DockSTARTer App Config" --menu "What would you like to do?" 0 0 0 "${DSAC_CONFIGOPTS[@]}" 3>&1 1>&2 2>&3 || echo "Cancel")
 
-    case "${CONFIGCHOICE}" in
+    case "${DSAC_CONFIGCHOICE}" in
         "Quick Setup ")
             # TODO: pre-configured apps
             # TODO: user inputs, as needed
-            warning "${CONFIGCHOICE} not yet available"
-            info "Returning to Main Menu."
-            return 1
+            error "${CONFIGCHOICE} not yet available"
             ;;
         "Full Setup ")
             # TODO: run_script 'dsac_menu_app_select'
             # TODO: user inputs, as needed
-            warning "${CONFIGCHOICE} not yet available"
-            info "Returning to Main Menu."
-            return 1
+            error "${CONFIGCHOICE} not yet available"
             ;;
         "Cancel")
             info "Returning to Main Menu."
             return 1
             ;;
         *)
-            error "Invalid Option"
+            error "Invalid DSAC Option"
             ;;
     esac
 }
