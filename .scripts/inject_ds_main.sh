d1ddf76a927c35d1ae4390728b685e8655cefd71
MagicalCodeMonkey <mwoodru@gmail.com>
2019-02-27 12:31:36 -0500

    Changes based on linting

--- .scripts/inject_ds_main.sh
+++ .scripts/inject_ds_main.sh
@@ -8,11 +8,11 @@ inject_ds_main() {
     local file_path
     file_path="${DETECTED_HOMEDIR}/.docker/${file}"
 
-    if [[ $(grep -c "# DSAC injected code" $file_path) = 0 ]]; then
+    if [[ $(grep -c "# DSAC injected code" "$file_path") = 0 ]]; then
         info "Injecting code into DockSTARTer ${file}"
 
         line_to_add_after="readonly DETECTED_HOMEDIR=\$(eval echo \"~\${DETECTED_UNAME}\" 2> /dev/null || true)"
-        line_number=$(($(grep -n "${line_to_add_after}" $file_path | sed 's/^\([0-9]\+\):.*$/\1/')+1))
+        line_number=$(($(grep -n "${line_to_add_after}" "$file_path" | sed 's/^\([0-9]\+\):.*$/\1/')+1))
 
         lines_to_add=(
             "# DSAC injected code"
@@ -21,14 +21,14 @@ inject_ds_main() {
             "# /DSAC injected code"
         )
 
-        for i in ${!lines_to_add[@]}; do
+        for i in "${!lines_to_add[@]}"; do
             line_to_add=${lines_to_add[$i]}
-            sed -i "${line_number}i $line_to_add" $file_path
+            sed -i "${line_number}i $line_to_add" "$file_path"
             line_number=$((line_number+1))
         done
 
         line_to_add_after="Failed to clone DockSTARTer repo to \${DETECTED_HOMEDIR}/.docker location."
-        line_number=$(($(grep -n "${line_to_add_after}" $file_path | sed 's/^\([0-9]\+\):.*$/\1/')+1))
+        line_number=$(($(grep -n "${line_to_add_after}" "$file_path" | sed 's/^\([0-9]\+\):.*$/\1/')+1))
 
         lines_to_add=(
             "\            # DSAC injected code"
@@ -39,9 +39,9 @@ inject_ds_main() {
             "\            # /DSAC injected code"
         )
 
-        for i in ${!lines_to_add[@]}; do
+        for i in "${!lines_to_add[@]}"; do
             line_to_add=${lines_to_add[$i]}
-            sed -i "${line_number}i $line_to_add" $file_path
+            sed -i "${line_number}i $line_to_add" "$file_path"
             line_number=$((line_number+1))
         done
     else
