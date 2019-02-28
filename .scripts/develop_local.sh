517e6f7f128c90fa9be5d68f012b7748b6103441
MagicalCodeMonkey <mwoodru@gmail.com>
2019-02-28 15:41:48 -0500

    Explicitly set default path

--- .scripts/develop_local.sh
+++ .scripts/develop_local.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 develop_local() {
     info "Updating DSAC from local development files: ${DETECTED_HOMEDIR}/${LOCAL_DIR}"
     cd "${DETECTED_HOMEDIR}/${LOCAL_DIR}"
-    find -iname '*.sh' -exec cp --parents {} "${DETECTED_DSACDIR}" \;
+    find . -iname '*.sh' -exec cp --parents {} "${DETECTED_DSACDIR}" \;
     cd "${DETECTED_HOMEDIR}"
 }
