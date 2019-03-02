50ab8a8cca37d957153e87962a40aead80dcfc18
MagicalCodeMonkey <mwoodru@gmail.com>
2019-03-01 23:55:30 -0500

    Update develop_local to use explicit paths instead of detections

--- .scripts/develop_local.sh
+++ .scripts/develop_local.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 develop_local() {
     info "Updating DSAC from local development files: ${DETECTED_HOMEDIR}/${LOCAL_DIR}"
-    cd "${DETECTED_HOMEDIR}/${LOCAL_DIR}"
-    find . -iname '*.sh' -exec cp --parents {} "${DETECTED_DSACDIR}" \;
-    cd "${DETECTED_HOMEDIR}"
+    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" "${DETECTED_DSACDIR}"
+    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.test" "${DETECTED_DSACDIR}"
+    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/compose" "${DETECTED_DSACDIR}"
 }
