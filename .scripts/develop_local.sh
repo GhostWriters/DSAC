bca09a9112aa388b148887a422746a1aca644de9
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-16 13:02:46 -0400

    Fix a typo in the develop_local script

--- .scripts/develop_local.sh
+++ .scripts/develop_local.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 develop_local() {
     info "Updating DSAC from local development files: ${DETECTED_HOMEDIR}/${LOCAL_DIR}"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" "${DETECTED_DSACDIR}"
-    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.test" "${DETECTED_DSACDIR}"
+    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.tests" "${DETECTED_DSACDIR}"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/compose" "${DETECTED_DSACDIR}"
 }
