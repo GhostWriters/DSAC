30f6b4bae548a1826d8ca6f8f5e191da21db291f
MagicalCodeMonkey <mwoodru@gmail.com>
2019-04-30 19:22:17 -0400

    Add main to develop local

--- .scripts/develop_local.sh
+++ .scripts/develop_local.sh
@@ -4,6 +4,7 @@ IFS=$'\n\t'
 
 develop_local() {
     info "Updating DSAC from local development files: ${DETECTED_HOMEDIR}/${LOCAL_DIR}"
+    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/main.sh" "${DETECTED_DSACDIR}/main.sh"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" "${DETECTED_DSACDIR}"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.tests" "${DETECTED_DSACDIR}"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/compose" "${DETECTED_DSACDIR}"
