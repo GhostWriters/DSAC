b0b716adcd4cb3b4b0a5ea77f644473613ad8bf6
MagicalCodeMonkey <mwoodru@gmail.com>
2019-06-25 16:10:47 -0400

    Updates to configure_movies_manager - Pull ports from array - Change names for added indexers/downloaders - Add/update nzbget configuration

--- .scripts/configure_movies_manager.sh
+++ .scripts/configure_movies_manager.sh
@@ -24,14 +24,14 @@ configure_movies_manager() {
         if [[ ${containers[hydra2]+true} == "true" ]]; then
             local radarr_hydra2_id
             local radarr_hydra2_settings
-            local hydra2_port=5076 #TODO: Make this pull from configuration file or db
+            local hydra2_port=${containers_ports[hydra2]} #TODO: Make this pull from configuration file or db
             local hydra2_base="/" #TODO: Make this pull from configuration file or db
             local hydra2_url="http://${LOCAL_IP}:${hydra2_port}${hydra2_base}"
 
             info "    Hydra2"
             debug "    Adding NZBHydra2 as an indexer, if needed..."
             $(sqlite3 "${db_path}" "INSERT INTO Indexers (Name,Implementation,Settings,ConfigContract,EnableRss,EnableSearch)
-                                    SELECT 'NZBHydra2','Newznab','{
+                                    SELECT 'NZBHydra2 (DSAC)','Newznab','{
                                             \"baseUrl\": \"${hydra2_url}\",
                                             \"multiLanguages\": [],
                                             \"apiKey\": \"${API_KEYS[hydra2]}\",
@@ -39,9 +39,9 @@ configure_movies_manager() {
                                             \"animeCategories\": [],
                                             \"removeYear\": false,
                                             \"searchByTitle\": false }','NewznabSettings',1,1
-                                    WHERE NOT EXISTS(SELECT 1 FROM Indexers WHERE name='NZBHydra2');")
+                                    WHERE NOT EXISTS(SELECT 1 FROM Indexers WHERE name='NZBHydra2 (DSAC)');")
             debug "    Get Hydra ID"
-            radarr_hydra2_id=$(sqlite3 "${db_path}" "SELECT id FROM Indexers WHERE Name='NZBHydra2'")
+            radarr_hydra2_id=$(sqlite3 "${db_path}" "SELECT id FROM Indexers WHERE Name='NZBHydra2 (DSAC)'")
             debug "    Hydra DB ID: ${radarr_hydra2_id}"
             # Get settings for Hydra
             radarr_hydra2_settings=$(sqlite3 "${db_path}" "SELECT Settings FROM Indexers WHERE id=$radarr_hydra2_id")
@@ -60,33 +60,48 @@ configure_movies_manager() {
         if [[ "${indexer_configured}" != "true" ]]; then
             warning "    None to configure."
         fi
-fatal "DEV"
+
         info "  - Updating Downloader settings"
         if [[ ${containers[nzbget]+true} == "true" ]]; then
-            info "    NZBget"
             local radarr_nzbget_id
-            radarr_nzbget_id=$(sqlite3 "${db_path}" "SELECT id FROM DownloadClients WHERE Name='NZBget'")
             local radarr_nzbget_settings
+            local radarr_port=${containers_ports[nzbget]} #TODO: Make this pull from configuration file or db
+            info "    NZBget"
+            debug "    Adding NZBget as an downloader, if needed..."
+            $(sqlite3 "${db_path}" "INSERT INTO DownloadClients (Enable,Name,Implementation,Settings,ConfigContract)
+                                    SELECT 1, 'NZBget (DSAC)','Nzbget','{
+                                            \"host\": \"${LOCAL_IP}\",
+                                            \"port\": ${radarr_port},
+                                            \"username\": \"nzbget\",
+                                            \"password\": \"tegbzn6789\",
+                                            \"movieCategory\": \"Movies\",
+                                            \"recentMoviePriority\": 0,
+                                            \"olderMoviePriority\": 0,
+                                            \"useSsl\": false,
+                                            \"addPaused\": false
+                                            }','NzbgetSettings'
+                                    WHERE NOT EXISTS(SELECT 1 FROM DownloadClients WHERE name='NZBget (DSAC)');")
+            debug "    Get NZBget ID"
+            radarr_nzbget_id=$(sqlite3 "${db_path}" "SELECT id FROM DownloadClients WHERE Name='NZBget (DSAC)'")
+            debug "    NZBget DB ID: ${radarr_nzbget_id}"
             radarr_nzbget_settings=$(sqlite3 "${db_path}" "SELECT Settings FROM DownloadClients WHERE id=$radarr_nzbget_id")
+            # Set host
+            debug "Setting host to: ${LOCAL_IP}"
+            radarr_nzbget_settings=$(sed 's/"host":.*/"host": "'${LOCAL_IP}'",/' <<< $radarr_nzbget_settings)
+            # Set port
+            debug "Setting host to: ${radarr_port}"
+            radarr_nzbget_settings=$(sed 's/"port":.*/"port": "'${radarr_port}'",/' <<< $radarr_nzbget_settings)
+            # Set username
+            debug "Setting username to: nzbget"
+            radarr_nzbget_settings=$(sed 's/"username":.*/"username": "nzbget",/' <<< $radarr_nzbget_settings)
+            # Set password
+            debug "Setting password to: tegbzn6789"
+            radarr_nzbget_settings=$(sed 's/"password":.*/"password": "tegbzn6789",/' <<< $radarr_nzbget_settings)
             # Change movieCategory to lowercase
-            debug "Setting movieCategory to: movies"
-            radarr_nzbget_settings=$(sed 's/"movieCategory":.*/"movieCategory": "movies",/' <<< $radarr_nzbget_settings)
+            debug "Setting movieCategory to: Movies"
+            radarr_nzbget_settings=$(sed 's/"movieCategory":.*/"movieCategory": "Movies",/' <<< $radarr_nzbget_settings)
             debug "Updating DB"
             sqlite3 "${db_path}" "UPDATE DownloadClients SET Settings='$radarr_nzbget_settings' WHERE id=$radarr_nzbget_id"
-            downloader_configured="true"
-        fi
-
-        if [[ ${containers[sabnzb]+true} == "true" ]]; then
-            info "    SABnzb"
-            local radarr_sabnzb_id
-            radarr_sabnzb_id=$(sqlite3 "${db_path}" "SELECT id FROM DownloadClients WHERE Name='SABnzbd'")
-            local radarr_sabnzb_settings
-            radarr_sabnzb_settings=$(sqlite3 "${db_path}" "SELECT Settings FROM DownloadClients WHERE id=$radarr_sabnzb_id")
-            # Set SABnzb API Key
-            debug "Setting API Key to: ${API_KEYS[sabnzbd]}"
-            radarr_sabnzb_settings=$(sed 's/"apiKey":.*/"apiKey": "'${API_KEYS[sabnzbd]}'",/' <<< $radarr_sabnzb_settings)
-            debug "Updating DB"
-            sqlite3 "${db_path}" "UPDATE DownloadClients SET Settings='$radarr_sabnzb_settings' WHERE id=$radarr_sabnzb_id"
             downloader_configured="true"
         fi
 
