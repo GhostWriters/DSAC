232b96a258aa380b16274e66a0061622423e002e
MagicalCodeMonkey <mwoodru@gmail.com>
2019-06-25 20:52:42 -0400

    Remove before replacing files

--- .scripts/develop_local.sh
+++ .scripts/develop_local.sh
@@ -5,7 +5,10 @@ IFS=$'\n\t'
 develop_local() {
     info "Updating DSAC from local development files: ${DETECTED_HOMEDIR}/${LOCAL_DIR}"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/main.sh" "${DETECTED_DSACDIR}/main.sh"
+    rm -r "${DETECTED_DSACDIR}/.scripts"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" "${DETECTED_DSACDIR}"
+    rm -r "${DETECTED_DSACDIR}/.tests"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.tests" "${DETECTED_DSACDIR}"
+    rm -r "${DETECTED_DSACDIR}/compose"
     cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/compose" "${DETECTED_DSACDIR}"
 }
