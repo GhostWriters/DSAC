064c8b5c3f5ba8f4adf18a0f49639658dc133af7
MagicalCodeMonkey <mwoodru@gmail.com>
2019-06-26 19:34:01 -0400

    Fix file copying issues

--- .scripts/develop_local.sh
+++ .scripts/develop_local.sh
@@ -4,11 +4,11 @@ IFS=$'\n\t'
 
 develop_local() {
     info "Updating DSAC from local development files: ${DETECTED_HOMEDIR}/${LOCAL_DIR}"
-    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/main.sh" "${DETECTED_DSACDIR}/main.sh"
-    rm -r "${DETECTED_DSACDIR}/.scripts"
-    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" "${DETECTED_DSACDIR}"
-    rm -r "${DETECTED_DSACDIR}/.tests"
-    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.tests" "${DETECTED_DSACDIR}"
-    rm -r "${DETECTED_DSACDIR}/compose"
-    cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/compose" "${DETECTED_DSACDIR}"
+    sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/main.sh" "${DETECTED_DSACDIR}/main.sh"
+    sudo rm -r "${DETECTED_DSACDIR}/.scripts"
+    sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" "${DETECTED_DSACDIR}"
+    sudo rm -r "${DETECTED_DSACDIR}/.tests"
+    sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.tests" "${DETECTED_DSACDIR}"
+    sudo rm -r "${DETECTED_DSACDIR}/compose"
+    sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/compose" "${DETECTED_DSACDIR}"
 }
