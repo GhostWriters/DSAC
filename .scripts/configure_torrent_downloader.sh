f630e46bec63405958eab7f1224f2e17636a772f
MagicalCodeMonkey <mwoodru@gmail.com>
2019-07-09 18:33:17 -0400

    Updates for qBittorrent - qBittorrent doesn't have all settings in config file on first boot - Make sure that @Invalid() isn't part of the list - stop container before changing the file

--- .scripts/configure_torrent_downloader.sh
+++ .scripts/configure_torrent_downloader.sh
@@ -31,37 +31,57 @@ configure_torrent_downloader() {
 
         local ip_addresses_new
         ip_addresses_new=("${network_subnet}/24")
+        debug "    ip_addresses_new=${ip_addresses_new}"
 
         local ip_addresses_current
-        ip_addresses_current="$(grep "AuthSubnetWhitelist=" "${config_path}")"
-        ip_addresses_current=${ip_addresses_current#*=}
-        ip_addresses_current=${ip_addresses_current// /}
+        if [[ $(grep -c "AuthSubnetWhitelist=" "${config_path}") > 0 ]]; then
+            ip_addresses_current="$(grep "AuthSubnetWhitelist=" "${config_path}")"
+            ip_addresses_current=${ip_addresses_current#*=}
+            ip_addresses_current=${ip_addresses_current// /}
 
-        IFS="," read -ra ip_addresses_current <<< "${ip_addresses_current}"
-        local match
-        for ip_current in "${ip_addresses_current[@]}"; do
-            match="false"
-            for ip_new in "${ip_addresses_new[@]}"; do
-                if [[ ${ip_current} == "${ip_new}" ]]; then
-                    match="true"
-                    break
+            debug "    Building new IP Address list"
+            IFS="," read -ra ip_addresses_current <<< "${ip_addresses_current}"
+            local match
+            for ip_current in "${ip_addresses_current[@]}"; do
+                match="false"
+                for ip_new in "${ip_addresses_new[@]}"; do
+                    if [[ ${ip_current} == "${ip_new}" ]]; then
+                        match="true"
+                        break
+                    fi
+                done
+
+                if [[ ${match} == "false" && ${ip_current} != "@Invalid()" ]]; then
+                    ip_addresses_new+=("${ip_current}")
                 fi
             done
+        else
+            ip_addresses_current=""
+        fi
+        info "  - Stopping ${container_name} to apply changes"
+        docker stop "${container_id}" > /dev/null
 
-            if [[ ${match} == "false" ]]; then
-                ip_addresses_new+=("${ip_current}")
-            fi
-        done
         debug "    ip_addresses_new=${ip_addresses_new[*]}"
         printf -v ip_addresses "%s," "${ip_addresses_new[@]}" 2> /dev/null
         debug "    ip_addresses=${ip_addresses}"
-        info "  - Enabling authentication bypass for local docker network..."
-        sed -i "s/AuthSubnetWhitelistEnabled=.*/AuthSubnetWhitelistEnabled=true/" "${config_path}"
         info "  - Adding docker network to the list..."
-        sed -i "s#AuthSubnetWhitelist=.*#AuthSubnetWhitelist=${ip_addresses}#" "${config_path}"
-
-        info "  - Restarting ${container_name} to apply changes"
-        docker restart "${container_id}" > /dev/null
+        if [[ $(grep -c "AuthSubnetWhitelist=" "${config_path}") > 0 ]]; then
+            debug "    Updating AuthSubnetWhitelist"
+            sed -i "s#AuthSubnetWhitelist=.*#AuthSubnetWhitelist=${ip_addresses}#" "${config_path}"
+        else
+            debug "    Adding AuthSubnetWhitelist"
+            echo "WebUI\AuthSubnetWhitelist=${ip_addresses}" >> "${config_path}"
+        fi
+        info "  - Enabling authentication bypass for local docker network..."
+        if [[ $(grep -c "AuthSubnetWhitelistEnabled=" "${config_path}") > 0 ]]; then
+            debug "    Updating AuthSubnetWhitelistEnabled"
+            sed -i "s/AuthSubnetWhitelistEnabled=.*/AuthSubnetWhitelistEnabled=true/" "${config_path}"
+        else
+            debug "    Adding AuthSubnetWhitelistEnabled"
+            echo "WebUI\AuthSubnetWhitelistEnabled=true" >> "${config_path}"
+        fi
+        info "  - Starting ${container_name}..."
+        docker start "${container_id}" > /dev/null
         info "  - Done"
     fi
 }
