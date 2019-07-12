0ff883c7ad286dfdf4b7439b69332fe7d8ed494c
MagicalCodeMonkey <mwoodru@gmail.com>
2019-07-11 20:57:50 -0400

    Linting...

--- .scripts/configure_subtitles_manager.sh
+++ .scripts/configure_subtitles_manager.sh
@@ -34,18 +34,18 @@ configure_subtitles_manager() {
         if [[ ${containers[sonarr]+true} == "true" ]]; then
             local sonarr_port
             sonarr_port=$(jq -r --arg port 8989 '.ports[$port]' <<< "${containers[sonarr]}")
-            crudini --set "${config_path}" sonarr apikey ${API_KEYS[sonarr]}
-            crudini --set "${config_path}" sonarr ip ${LOCAL_IP}
-            crudini --set "${config_path}" sonarr port ${sonarr_port}
+            crudini --set "${config_path}" sonarr apikey "${API_KEYS[sonarr]}"
+            crudini --set "${config_path}" sonarr ip "${LOCAL_IP}"
+            crudini --set "${config_path}" sonarr port "${sonarr_port}"
             crudini --set "${config_path}" general use_sonarr true
         fi
 
         if [[ ${containers[radarr]+true} == "true" ]]; then
             local radarr_port
             radarr_port=$(jq -r --arg port 7878 '.ports[$port]' <<< "${containers[radarr]}")
-            crudini --set "${config_path}" radarr apikey ${API_KEYS[radarr]}
-            crudini --set "${config_path}" radarr ip ${LOCAL_IP}
-            crudini --set "${config_path}" radarr port ${radarr_port}
+            crudini --set "${config_path}" radarr apikey "${API_KEYS[radarr]}"
+            crudini --set "${config_path}" radarr ip "${LOCAL_IP}"
+            crudini --set "${config_path}" radarr port "${radarr_port}"
             crudini --set "${config_path}" general use_radarr true
         fi
 
