b9d868f6ca4474628663e64cf668c1b228e7cfd1
MagicalCodeMonkey <mwoodru@gmail.com>
2019-07-25 21:29:25 -0400

    Adjust set_permissions for travis

--- .scripts/set_permissions.sh
+++ .scripts/set_permissions.sh
@@ -28,11 +28,6 @@ set_permissions() {
     CH_PUID=${2:-$DETECTED_PUID}
     local CH_PGID
     CH_PGID=${3:-$DETECTED_PGID}
-    if [[ ${CI:-} == true ]] && [[ ${TRAVIS:-} == true ]]; then
-        info "Overriding PUID and PGID for Travis."
-        CH_PUID=${DETECTED_UNAME}
-        CH_PGID=${DETECTED_UGROUP}
-    fi
     info "Taking ownership of ${CH_PATH} for user ${CH_PUID} and group ${CH_PGID}"
     chown -R "${CH_PUID}":"${CH_PGID}" "${CH_PATH}" > /dev/null 2>&1 || true
     info "Setting file and folder permissions in ${CH_PATH}"
