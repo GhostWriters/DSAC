f2b3e8b4682e30e0dfc1e865b38396458b5268e7
MagicalCodeMonkey <MagicalCodeMonkey@users.noreply.github.com>
2020-02-22 11:51:38 -0500

    Updates for builds and github things (#27)
    * Update CI
    * Update Issue/PR handling
    * Linting for new CI

--- .develop/develop.sh
+++ .develop/develop.sh
@@ -41,7 +41,7 @@ get_scriptname() {
     local SOURCE
     local DIR
     SOURCE="${BASH_SOURCE[0]:-$0}" # https://stackoverflow.com/questions/35006457/choosing-between-0-and-bash-source
-    while [[ -L ${SOURCE} ]]; do # resolve ${SOURCE} until the file is no longer a symlink
+    while [[ -L ${SOURCE} ]]; do   # resolve ${SOURCE} until the file is no longer a symlink
         DIR="$(cd -P "$(dirname "${SOURCE}")" > /dev/null && pwd)"
         SOURCE="$(readlink "${SOURCE}")"
         [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}" # if ${SOURCE} was a relative symlink, we need to resolve it relative to the path where the symlink file was located
