d1fce704ef033bb99f74ce8ef3bd77be340a2376
MagicalCodeMonkey <mwoodru@gmail.com>
2020-05-10 01:55:28 -0400

    Convert to use yml - Change variable names - Update outputs - Restructure/refactor

--- .scripts/get_docker_containers.sh
+++ .scripts/get_docker_containers.sh
@@ -6,57 +6,63 @@ get_docker_containers() {
     if [[ $(docker ps -q | wc -l) -gt 1 ]]; then
         notice "Getting docker container information"
         while IFS= read -r line; do
-            local container_id=${line}
-            local container_image
-            local container_name
-            local TEMP
-            container_image=$(docker container inspect -f '{{ .Config.Image }}' "${container_id}")
-            container_name=$(docker container inspect -f '{{ .Name }}' "${container_id}")
-            container_name=${container_name//\//}
+            local CONTAINER_ID=${line}
+            local CONTAINER_IMAGE
+            CONTAINER_IMAGE=$(docker container inspect -f '{{ .Config.Image }}' "${CONTAINER_ID}")
+            local CONTAINER_NAME
+            CONTAINER_NAME=$(docker container inspect -f '{{ .Name }}' "${CONTAINER_ID}")
+            CONTAINER_NAME=${CONTAINER_NAME//\//}
+            local CONTAINER_YML
+            CONTAINER_YML="services.${CONTAINER_NAME}.labels[com.dockstarter.dsac]"
+            local CONTAINER_YML_FILE
+            CONTAINER_YML_FILE="${DETECTED_DSACDIR}/.apps/${CONTAINER_NAME}/${CONTAINER_NAME}.labels.dsac.yml"
 
-            if [[ ${containers[${container_name}]+true} == "true" ]]; then
-                info "- ${container_name} already exists..."
-            else
-                info "- Adding ${container_name} to list."
-                if [[ -f "${DETECTED_DSACDIR}/.data/${container_name}.json" ]]; then
-                    containers[${container_name}]=$(cat "${DETECTED_DSACDIR}/.data/${container_name}.json")
+            notice "${CONTAINER_NAME}"
+            if [[ -d "${DETECTED_DSACDIR}/.apps/${CONTAINER_NAME}" ]]; then
+                DOCKER_NAME=$(yq-go r "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_name")
+                info "DOCKER_NAME='${DOCKER_NAME}'"
+                if [[ $(yq-go r "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_name") ]]; then
+                    info "Updating information..."
                 else
-                    containers[${container_name}]=$(cat "${DETECTED_DSACDIR}/.data/template.json")
+                    info "Adding information..."
                 fi
-                containers[${container_name}]=$(jq --arg var "${container_id}" '.container_id = $var' <<< "${containers[${container_name}]}")
-                containers[${container_name}]=$(jq --arg var "${container_image}" '.container_image = $var' <<< "${containers[${container_name}]}")
-                containers[${container_name}]=$(jq '.is_docker = "true"' <<< "${containers[${container_name}]}")
-
-                # Get container config path
-                info "  Getting ${container_name} config path."
-                mapfile -t TEMP < <(docker container inspect "${container_id}" | jq '.[0].Mounts[] | tostring')
-                for i in "${TEMP[@]}"; do
-                    local mounts
-                    mounts=$(jq 'fromjson | .Destination' <<< "$i")
-                    mounts=${mounts//\"/}
-                    if [[ ${mounts} == "/config" ]]; then
-                        config_source=$(jq 'fromjson | .Source' <<< "$i")
-                        config_source=${config_source//\"/}
-                        debug "  config_source=${config_source}"
-                        containers[${container_name}]=$(jq --arg var "${config_source}" '.config.source = $var' <<< "${containers[${container_name}]}")
-                    fi
-                done
-                # Get container ports
-                info "  Getting ${container_name} port(s)."
-                local port_original
-                local port_configured
-                mapfile -t TEMP < <(docker port "${container_id}")
-                for port_mapping in "${TEMP[@]}"; do
-                    port_original=$(awk '{split($1,a,"/"); print a[1]}' <<< "${port_mapping}")
-                    port_configured=$(awk '{split($3,a,":"); print a[2]}' <<< "${port_mapping}")
-                    containers[${container_name}]=$(jq --arg port1 "${port_original}" --arg port2 "${port_configured}" '.ports[$port1] = $port2' <<< "${containers[${container_name}]}")
-                done
-                if [[ ! -d "${DETECTED_DSACDIR}/.data/" ]]; then
-                    mkdir -p "${DETECTED_DSACDIR}/.data/"
-                fi
-                #debug "containers[${container_name}]=${containers[${container_name}]}"
-                echo "${containers[${container_name}]}" > "${DETECTED_DSACDIR}/.data/${container_name}.json"
+            else
+                warn "Could not find file for ${CONTAINER_NAME}. Skipping..."
+                continue
             fi
+            # Write container information to yml
+            yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_name" "${CONTAINER_NAME}"
+            yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_id" "${CONTAINER_ID}"
+            yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_image" "${CONTAINER_IMAGE}"
+
+            # Get container config path
+            info "Getting ${CONTAINER_NAME} config path."
+            mapfile -t MOUNTS < <(docker container inspect "${CONTAINER_ID}" | jq '.[0].Mounts[] | tostring')
+            for i in "${MOUNTS[@]}"; do
+                local MOUNT
+                MOUNT=$(jq 'fromjson | .Destination' <<< "$i")
+                MOUNT=${MOUNT//\"/}
+                if [[ ${MOUNT} == "/config" ]]; then
+                    local CONFIG_SOURCE
+                    CONFIG_SOURCE=$(jq 'fromjson | .Source' <<< "$i")
+                    CONFIG_SOURCE=${CONFIG_SOURCE//\"/}
+                    debug "CONFIG_SOURCE=${CONFIG_SOURCE}"
+                    yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.config.source" "${CONFIG_SOURCE}"
+                fi
+            done
+
+            # Get container ports
+            info "Getting ${CONTAINER_NAME} port(s)."
+            local PORT_ORIGINAL
+            local PORT_CONFIGURED
+            mapfile -t PORTS < <(docker port "${CONTAINER_ID}")
+            for PORT_MAPPING in "${PORTS[@]}"; do
+                PORT_ORIGINAL=$(awk '{split($1,a,"/"); print a[1]}' <<< "${PORT_MAPPING}")
+                PORT_CONFIGURED=$(awk '{split($3,a,":"); print a[2]}' <<< "${PORT_MAPPING}")
+                yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.ports.${PORT_ORIGINAL}" "${PORT_CONFIGURED}"
+            done
+
+            containers[${CONTAINER_NAME}]=true
         done < <(sudo docker ps -q)
     else
         error "You don't have any running docker containers."
