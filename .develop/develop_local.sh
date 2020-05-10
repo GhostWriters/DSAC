03900f38d8c7d4f1432a3a855a95994eb84ad267
MagicalCodeMonkey <mwoodru@gmail.com>
2020-05-10 01:28:15 -0400

    Update develop scripts

--- .develop/develop_local.sh
+++ .develop/develop_local.sh
@@ -5,17 +5,36 @@ IFS=$'\n\t'
 develop_local() {
     info "Updating DSAC from local development files: ${DETECTED_HOMEDIR}/${LOCAL_DIR}"
 
+    info "Updating ${DETECTED_DSACDIR}/main.sh"
     sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/main.sh" "${DETECTED_DSACDIR}/main.sh"
-    sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/dsac_manifest.csv" "${DETECTED_DSACDIR}/dsac_manifest.csv"
+    #sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/dsac_manifest.csv" "${DETECTED_DSACDIR}/dsac_manifest.csv"
+
+    if [[ -d "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.develop" ]]; then
+        info "Updating ${DETECTED_DSACDIR}/.develop"
+        if [[ -d "${DETECTED_DSACDIR}/.develop" ]]; then
+            sudo rm -r "${DETECTED_DSACDIR}/.develop"
+        fi
+        sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.develop" "${DETECTED_DSACDIR}"
+    fi
 
     if [[ -d "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" ]]; then
+        info "Updating ${DETECTED_DSACDIR}/.scripts"
         if [[ -d "${DETECTED_DSACDIR}/.scripts" ]]; then
             sudo rm -r "${DETECTED_DSACDIR}/.scripts"
         fi
         sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.scripts" "${DETECTED_DSACDIR}"
     fi
 
+    if [[ -d "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.apps" ]]; then
+        info "Updating ${DETECTED_DSACDIR}/.apps"
+        if [[ -d "${DETECTED_DSACDIR}/.apps" ]]; then
+            sudo rm -r "${DETECTED_DSACDIR}/.apps"
+        fi
+        sudo cp -r "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.apps" "${DETECTED_DSACDIR}"
+    fi
+
     if [[ -d "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.data" ]]; then
+        info "Updating ${DETECTED_DSACDIR}/.data"
         if [[ -d "${DETECTED_DSACDIR}/.data" ]]; then
             sudo rm -r "${DETECTED_DSACDIR}/.data"
         fi
@@ -23,6 +42,7 @@ develop_local() {
     fi
 
     if [[ -d "${DETECTED_HOMEDIR}/${LOCAL_DIR}/.tests" ]]; then
+        info "Updating ${DETECTED_DSACDIR}/.tests"
         if [[ -d "${DETECTED_DSACDIR}/.tests" ]]; then
             sudo rm -r "${DETECTED_DSACDIR}/.tests"
         fi
@@ -30,6 +50,7 @@ develop_local() {
     fi
 
     if [[ -d "${DETECTED_HOMEDIR}/${LOCAL_DIR}/compose" ]]; then
+        info "Updating ${DETECTED_DSACDIR}/compose"
         if [[ -d "${DETECTED_DSACDIR}/compose" ]]; then
             sudo rm -r "${DETECTED_DSACDIR}/compose"
         fi
