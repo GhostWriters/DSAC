36abf8fb0f6b75dfdd1ed19e3f09691b38fb95af
MagicalCodeMonkey <mwoodru@gmail.com>
2020-05-13 23:55:08 -0400

    Update get_docker_containers script - Change to using yml in .data directory instead of overwriting base YML - Update some output and comments

--- .scripts/get_docker_containers.sh
+++ .scripts/get_docker_containers.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 get_docker_containers() {
     if [[ $(docker ps -q | wc -l) -gt 1 ]]; then
-        notice "Getting docker container information"
+        notice "Scanning Docker for supported containers"
         while IFS= read -r line; do
             local CONTAINER_ID=${line}
             local CONTAINER_IMAGE
@@ -14,20 +14,28 @@ get_docker_containers() {
             CONTAINER_NAME=${CONTAINER_NAME//\//}
             local CONTAINER_YML
             CONTAINER_YML="services.${CONTAINER_NAME}.labels[com.dockstarter.dsac]"
+            local CONTAINER_BASE_YML_FILE
+            CONTAINER_BASE_YML_FILE="${DETECTED_DSACDIR}/.apps/${CONTAINER_NAME}/${CONTAINER_NAME}.labels.dsac.yml"
             local CONTAINER_YML_FILE
-            CONTAINER_YML_FILE="${DETECTED_DSACDIR}/.apps/${CONTAINER_NAME}/${CONTAINER_NAME}.labels.dsac.yml"
+            CONTAINER_YML_FILE="${DETECTED_DSACDIR}/.data/apps/${CONTAINER_NAME}/${CONTAINER_NAME}.yml"
 
-            notice "${CONTAINER_NAME}"
+            info "${CONTAINER_NAME}"
+            debug "CONTAINER_YML=${CONTAINER_YML}"
+            debug "CONTAINER_BASE_YML_FILE=${CONTAINER_BASE_YML_FILE}"
+            debug "CONTAINER_YML_FILE=${CONTAINER_YML_FILE}"
             if [[ -d "${DETECTED_DSACDIR}/.apps/${CONTAINER_NAME}" ]]; then
-                DOCKER_NAME=$(yq-go r "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_name")
-                info "DOCKER_NAME='${DOCKER_NAME}'"
-                if [[ $(yq-go r "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_name") ]]; then
+                # Check if the information needs to be added or updated
+                if [[ -f "${CONTAINER_YML_FILE}" ]]; then
                     info "Updating information..."
                 else
                     info "Adding information..."
+                    if [[ ! -d "${DETECTED_DSACDIR}/.data/apps/${CONTAINER_NAME}" ]]; then
+                        mkdir -p "${DETECTED_DSACDIR}/.data/apps/${CONTAINER_NAME}"
+                    fi
+                    cp "${CONTAINER_BASE_YML_FILE}" "${CONTAINER_YML_FILE}"
                 fi
             else
-                warn "Could not find file for ${CONTAINER_NAME}. Skipping..."
+                info "${CONTAINER_NAME} not supported. Skipping..."
                 continue
             fi
             # Write container information to yml
@@ -35,19 +43,22 @@ get_docker_containers() {
             yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_id" "${CONTAINER_ID}"
             yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.docker.container_image" "${CONTAINER_IMAGE}"
 
-            # Get container config path
+            # Get container volume mounts
             info "Getting ${CONTAINER_NAME} config path."
             mapfile -t MOUNTS < <(docker container inspect "${CONTAINER_ID}" | jq '.[0].Mounts[] | tostring')
             for i in "${MOUNTS[@]}"; do
                 local MOUNT
                 MOUNT=$(jq 'fromjson | .Destination' <<< "$i")
                 MOUNT=${MOUNT//\"/}
+                # Get container config path
                 if [[ ${MOUNT} == "/config" ]]; then
                     local CONFIG_SOURCE
                     CONFIG_SOURCE=$(jq 'fromjson | .Source' <<< "$i")
                     CONFIG_SOURCE=${CONFIG_SOURCE//\"/}
                     debug "CONFIG_SOURCE=${CONFIG_SOURCE}"
                     yq-go w -i "${CONTAINER_YML_FILE}" "${CONTAINER_YML}.config.source" "${CONFIG_SOURCE}"
+                    # config path found. Time to move on.
+                    break
                 fi
             done
 
