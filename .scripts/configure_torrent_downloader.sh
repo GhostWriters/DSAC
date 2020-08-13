b2682d94485318202f5e9c943bea18993866f15e
MagicalCodeMonkey <magicalcodemonkey@gmail.com>
2020-08-13 16:23:32 -0400

    Fix variables and some YML handling

--- .scripts/configure_torrent_downloader.sh
+++ .scripts/configure_torrent_downloader.sh
@@ -11,56 +11,58 @@ configure_torrent_downloader() {
 
     # shellcheck disable=SC2154,SC2001
     if [[ $(run_script 'yml_get' "${APPNAME}" "${APP_CONTAINER_YML}.docker.running") == "true" ]]; then
+        local CONTAINER_ID
+        CONTAINER_ID=$(run_script 'yml_get' "${APPNAME}" "${APP_CONTAINER_YML}.docker.container_id")
         if [[ ${APPNAME} == "qbittorrent" ]]; then
-            local container_network
-            container_network=$(docker container inspect "${container_id}" | jq '.[0].NetworkSettings.Networks | to_entries[].key' | awk '{gsub("\"",""); print}')
-            debug "    container_network=${container_network}"
-            local network_subnet
-            network_subnet=$(docker network inspect "${container_network}" | jq '.[0].IPAM.Config[0].Subnet' | awk '{gsub("\"",""); split($0,a,"/"); print a[1]}')
-            debug "    network_subnet=${network_subnet}"
+            local CONTAINER_NETWORK
+            CONTAINER_NETWORK=$(docker container inspect "${CONTAINER_ID}" | jq '.[0].NetworkSettings.Networks | to_entries[].key' | awk '{gsub("\"",""); print}')
+            debug "    CONTAINER_NETWORK=${CONTAINER_NETWORK}"
+            local NETWORK_SUBNET
+            NETWORK_SUBNET=$(docker network inspect "${CONTAINER_NETWORK}" | jq '.[0].IPAM.Config[0].Subnet' | awk '{gsub("\"",""); split($0,a,"/"); print a[1]}')
+            debug "    NETWORK_SUBNET=${NETWORK_SUBNET}"
 
-            local ip_addresses_new
-            ip_addresses_new=("${network_subnet}/24")
-            debug "    ip_addresses_new=${ip_addresses_new[*]}"
+            local IP_ADDRESSES_NEW
+            IP_ADDRESSES_NEW=("${NETWORK_SUBNET}/24")
+            debug "    IP_ADDRESSES_NEW=${IP_ADDRESSES_NEW[*]}"
 
-            local ip_addresses_current
+            local IP_ADDRESSES_CURRENT
             #TODO: Change all "grep -c ... -gt 0" to use "grep -p"
             if [[ $(grep -c "AuthSubnetWhitelist=" "${APP_CONFIG_PATH}") -gt 0 ]]; then
-                ip_addresses_current="$(grep "AuthSubnetWhitelist=" "${APP_CONFIG_PATH}")"
-                ip_addresses_current=${ip_addresses_current#*=}
-                ip_addresses_current=${ip_addresses_current// /}
+                IP_ADDRESSES_CURRENT="$(grep "AuthSubnetWhitelist=" "${APP_CONFIG_PATH}")"
+                IP_ADDRESSES_CURRENT=${IP_ADDRESSES_CURRENT#*=}
+                IP_ADDRESSES_CURRENT=${IP_ADDRESSES_CURRENT// /}
 
                 debug "    Building new IP Address list"
-                IFS="," read -ra ip_addresses_current <<< "${ip_addresses_current}"
-                local match
-                for ip_current in "${ip_addresses_current[@]}"; do
-                    match="false"
-                    for ip_new in "${ip_addresses_new[@]}"; do
-                        if [[ ${ip_current} == "${ip_new}" ]]; then
-                            match="true"
+                IFS="," read -ra IP_ADDRESSES_CURRENT <<< "${IP_ADDRESSES_CURRENT}"
+                local MATCH
+                for IP_CURRENT in "${IP_ADDRESSES_CURRENT[@]}"; do
+                    MATCH="false"
+                    for IP_NEW in "${IP_ADDRESSES_NEW[@]}"; do
+                        if [[ ${IP_CURRENT} == "${IP_NEW}" ]]; then
+                            MATCH="true"
                             break
                         fi
                     done
 
-                    if [[ ${match} == "false" && ${ip_current} != "@Invalid()" ]]; then
-                        ip_addresses_new+=("${ip_current}")
+                    if [[ ${MATCH} == "false" && ${IP_CURRENT} != "@Invalid()" ]]; then
+                        IP_ADDRESSES_NEW+=("${IP_CURRENT}")
                     fi
                 done
             else
-                ip_addresses_current=()
+                IP_ADDRESSES_CURRENT=()
             fi
 
-            debug "    ip_addresses_new=${ip_addresses_new[*]}"
-            printf -v ip_addresses "%s," "${ip_addresses_new[@]}" 2> /dev/null
-            debug "    ip_addresses=${ip_addresses}"
+            debug "    IP_ADDRESSES_NEW=${IP_ADDRESSES_NEW[*]}"
+            printf -v IP_ADDRESSES "%s," "${IP_ADDRESSES_NEW[@]}" 2> /dev/null
+            debug "    IP_ADDRESSES=${IP_ADDRESSES}"
             info "    - Adding docker network to the list..."
             #TODO: Change all "grep -c ... -gt 0" to use "grep -p"
             if [[ $(grep -c "AuthSubnetWhitelist=" "${APP_CONFIG_PATH}") -gt 0 ]]; then
                 debug "    Updating AuthSubnetWhitelist"
-                sed -i "s#AuthSubnetWhitelist=.*#AuthSubnetWhitelist=${ip_addresses}#" "${APP_CONFIG_PATH}"
+                sed -i "s#AuthSubnetWhitelist=.*#AuthSubnetWhitelist=${IP_ADDRESSES}#" "${APP_CONFIG_PATH}"
             else
                 debug "    Adding AuthSubnetWhitelist"
-                echo "WebUI\AuthSubnetWhitelist=${ip_addresses}" >> "${APP_CONFIG_PATH}"
+                echo "WebUI\AuthSubnetWhitelist=${IP_ADDRESSES}" >> "${APP_CONFIG_PATH}"
             fi
             info "    - Enabling authentication bypass for local docker network..."
             #TODO: Change all "grep -c ... -gt 0" to use "grep -p"
