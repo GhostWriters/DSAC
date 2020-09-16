c05ded2c450bee6cec4ef5a3819ff9a1446ed7f8
Eric Nemchik <eric@nemchik.com>
2020-09-16 12:27:08 -0500

    Fix shfmt

--- .scripts/pm_pacman_install.sh
+++ .scripts/pm_pacman_install.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 pm_pacman_install() {
     info "Installing dependencies."
-    pacman -Sy --noconfirm curl git grep libnewt sed sqlite3 crudini jq> /dev/null 2>&1 || fatal "Failed to install dependencies using pacman."
+    pacman -Sy --noconfirm curl git grep libnewt sed sqlite3 crudini jq > /dev/null 2>&1 || fatal "Failed to install dependencies using pacman."
 }
 
 test_pm_pacman_install() {
